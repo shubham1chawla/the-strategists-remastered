f4c63d8444d5be83dcbe957b1fdbf552ca088d0a
Shubham Chawla <shubham_chawla@live.com>
2023-01-23 01:31:32 -0700

    setting up server

---
+++ server/src/test/java/com/strategists/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strategists.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
