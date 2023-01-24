078e4bd9ddeb0016b8ea207267945a483b32c68c
zyfers <himanshusharma7296@gmail.com>
2023-01-23 21:56:31 -0700

    Added routing and redux

---
+++ web/src/components/NotFound.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class NotFound extends Component {
+  static propTypes: {};
+
+  render() {
+    return (
+      <div>
+        <h1>Oops Page Not Found</h1>
+      </div>
+    );
+  }
+}
+
+NotFound.propTypes = {};
+
+export default NotFound;
