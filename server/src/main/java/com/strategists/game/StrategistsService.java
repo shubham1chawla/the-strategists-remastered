ff94f96e8e480c525a957a9c30fc9e2de34669cf
Shubham Chawla <shubham_chawla@live.com>
2023-02-09 15:28:14 -0700

    added devtools dependency, cleaned main, and updated dev h2 url

--- server/src/main/java/com/strategists/game/StrategistsService.java
+++ server/src/main/java/com/strategists/game/StrategistsService.java
@@ -1,22 +1,13 @@
 package com.strategists.game;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.log4j.Log4j2;
-
-@Log4j2
 @SpringBootApplication
-public class StrategistsService implements CommandLineRunner {
+public class StrategistsService {
 
 	public static void main(String... args) {
 		SpringApplication.run(StrategistsService.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		log.info("We are up guys!");
 	}
 
 }
