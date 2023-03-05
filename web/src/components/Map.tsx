41434aaf7a24bac6bd1ca7b56b9fb556415e9112
Himanshu Sharma <himanshusharma7296@gmail.com>
2023-03-04 23:34:07 -0700

    added land api

--- web/src/components/Map.tsx
+++ web/src/components/Map.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Map = () => {
+  const [lands, setLands] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8090/api/lands').then((res) => {
+      setLands(res.data);
+    });
+  }, []);
+
   return (
     <div>
-      <h1>This is Map Component</h1>
+      <h2>This is Map Component</h2>
+      {lands.map((land, i) => (
+        <h2 key={i}>{land.name}</h2>
+      ))}
     </div>
   );
 };
