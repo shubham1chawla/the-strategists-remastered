a6b5a9c7b8d5ed4b24d0086ace4f87de90309712
Shubham Chawla <shubham@Shubhams-Laptop.local>
2023-05-15 13:06:37 +0530

    adding password support for players

--- server/src/main/java/com/strategists/game/controller/PlayerController.java
+++ server/src/main/java/com/strategists/game/controller/PlayerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,11 @@ public class PlayerController {
 	@GetMapping
 	public List<Player> getPlayers() {
 		return playerService.getPlayers();
+	}
+
+	@GetMapping("/{playerId}/password")
+	public String getPassword(@PathVariable Long playerId) {
+		return playerService.getPlayerById(playerId).getPassword();
 	}
 
 	@PostMapping
