e5df0712f60796c918a85dd974bfabebf41cdf02
Shubham Chawla <shubham@Shubhams-Laptop.local>
2023-05-31 22:55:04 +0530

    fixed login error messages

--- server/src/main/java/com/strategists/game/controller/AuthController.java
+++ server/src/main/java/com/strategists/game/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.strategists.game.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,18 +12,26 @@ import com.strategists.game.request.AuthPlayerRequest;
 import com.strategists.game.service.AuthService;
 import com.strategists.game.service.AuthService.Type;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
 	@Autowired
 	private AuthService authService;
-	
+
 	@PostMapping
-	public Type authenticate(@RequestBody AuthPlayerRequest request) {
+	public ResponseEntity<Type> authenticate(@RequestBody AuthPlayerRequest request) {
 		Assert.hasText(request.getUsername(), "Username shouldn't be empty!");
 		Assert.hasText(request.getPassword(), "Password shouldn't be empty!");
-		return authService.authenticate(request.getUsername(), request.getPassword());
+		try {
+			return ResponseEntity.ok(authService.authenticate(request.getUsername(), request.getPassword()));
+		} catch (Exception ex) {
+			log.error("Authentication attempt failed! Message: {}", ex.getMessage());
+			return ResponseEntity.notFound().build();
+		}
 	}
-	
+
 }
