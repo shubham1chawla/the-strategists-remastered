8a50053829684d16b5a2cbf87b84603ceeebf229
Shubham Chawla <shubham@Shubhams-Laptop.local>
2023-09-19 00:52:45 -0700

    disabling invest modal on low cash

--- web/src/components/Actions.tsx
+++ web/src/components/Actions.tsx
@@ -4,6 +4,7 @@ import {
   LoadingOutlined,
   StepForwardOutlined,
   StockOutlined,
+  StopOutlined,
 } from '@ant-design/icons';
 import { Button, Dropdown, Space } from 'antd';
 import { useEffect, useState } from 'react';
@@ -15,18 +16,35 @@ import axios from 'axios';
 export const Actions = () => {
   const { players, lands } = useSelector((state: State) => state.lobby);
   const { username } = useSelector((state: State) => state.user);
-  const [disabled, setDisabled] = useState(true);
+
+  const [isWaiting, setWaiting] = useState(true);
+  const [isInvestmentDisabled, setInvestmentDisabled] = useState(true);
   const [investText, setInvestText] = useState('');
   const [showModal, setShowModal] = useState(false);
 
-  // finding user in lobby's players
+  // Finding user in lobby's players
   const player = players.find((p) => p.username === username);
   const turnPlayer = players.find((p) => p.turn);
   const land = player ? lands[player.index] : undefined;
 
   useEffect(() => {
-    setDisabled(!player?.turn);
-    setInvestText(`Invest in ${land?.name}`);
+    if (!player || !land) {
+      return;
+    }
+
+    // Checking if investment is allowed
+    if (!land.marketValue || player.cash < 0.01 * land.marketValue) {
+      setInvestText(
+        !land.marketValue
+          ? `Cannot invest in ${land?.name}`
+          : `Not enough cash to invest!`
+      );
+      setInvestmentDisabled(true);
+    } else {
+      setInvestText(`Invest in ${land?.name}`);
+      setInvestmentDisabled(false);
+    }
+    setWaiting(!player.turn);
   }, [player, land]);
 
   return (
@@ -39,55 +57,94 @@ export const Actions = () => {
         onCancel={() => setShowModal(false)}
       />
       <div className="strategists-actions">
-        {player?.state === 'BANKRUPT'
-          ? renderBankurptButton()
-          : disabled
-          ? renderDisabledButton(turnPlayer)
-          : renderActionButtons(investText, () => setShowModal(true))}
+        {player?.state === 'BANKRUPT' ? (
+          <BankruptPrompt />
+        ) : isWaiting ? (
+          <WaitingPrompt turnPlayer={turnPlayer} />
+        ) : (
+          <ActionButtons
+            investText={investText}
+            onInvestClick={() => setShowModal(true)}
+            isInvestmentDisabled={isInvestmentDisabled}
+          />
+        )}
       </div>
     </>
   );
 };
 
-const renderBankurptButton = () => (
+/**
+ * -----  BANKRUPT PROMPT BELOW  -----
+ */
+
+const BankruptPrompt = () => (
   <Button disabled size="large" type="primary" icon={<AuditOutlined />}>
     You are declared bankrupt!
   </Button>
 );
 
-const renderDisabledButton = (turnPlayer?: Player) => (
-  <Button disabled size="large" type="primary" icon={<LoadingOutlined />}>
-    {turnPlayer
-      ? `${turnPlayer.username}'s turn to invest`
-      : 'The Strategists not started!'}
-  </Button>
-);
+/**
+ * -----  WAITING PROMPT BELOW  -----
+ */
 
-const renderActionButtons = (investText: string, onClick: () => void) => (
-  <>
-    <Space.Compact size="large">
-      <Button type="primary" icon={<StockOutlined />} onClick={onClick}>
-        {investText}
-      </Button>
-      <Button
-        icon={<StepForwardOutlined />}
-        onClick={() => axios.put('/api/game')}
-      >
-        Skip
-      </Button>
-      <Dropdown
-        menu={{
-          items: [
-            {
-              key: '1',
-              label: 'Apply Cheat',
-            },
-          ],
-        }}
-        trigger={['click']}
-      >
-        <Button icon={<EllipsisOutlined />} />
-      </Dropdown>
-    </Space.Compact>
-  </>
-);
+interface WaitingPromptProps {
+  turnPlayer?: Player;
+}
+
+const WaitingPrompt = (props: WaitingPromptProps) => {
+  const { turnPlayer } = props;
+  return (
+    <Button disabled size="large" type="primary" icon={<LoadingOutlined />}>
+      {turnPlayer
+        ? `${turnPlayer.username}'s turn to invest`
+        : 'The Strategists not started!'}
+    </Button>
+  );
+};
+
+/**
+ * -----  ACTION BUTTONS BELOW  -----
+ */
+
+interface ActionButtonsProps {
+  investText: string;
+  onInvestClick: () => void;
+  isInvestmentDisabled: boolean;
+}
+
+const ActionButtons = (props: ActionButtonsProps) => {
+  const { investText, onInvestClick, isInvestmentDisabled } = props;
+  return (
+    <>
+      <Space.Compact size="large">
+        <Button
+          type="primary"
+          disabled={isInvestmentDisabled}
+          icon={isInvestmentDisabled ? <StopOutlined /> : <StockOutlined />}
+          onClick={onInvestClick}
+        >
+          {investText}
+        </Button>
+        <Button
+          icon={<StepForwardOutlined />}
+          onClick={() => axios.put('/api/game')}
+        >
+          Skip
+        </Button>
+        <Dropdown
+          menu={{
+            items: [
+              {
+                key: '1',
+                label: 'Apply Cheat',
+              },
+            ],
+          }}
+          trigger={['click']}
+        >
+          <Button icon={<EllipsisOutlined />} />
+        </Dropdown>
+      </Space.Compact>
+    </>
+  );
+};
