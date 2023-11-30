aa3d00ec5d7ed75453354e9415d2e57d6fc64042
Shubham Chawla <shubham_chawla@live.com>
2023-11-30 04:45:28 -0700

    refactoring model project

--- model/main.py
+++ model/main.py
@@ -1,54 +1,24 @@
 #!./env/bin/python
 
 from argparse import ArgumentParser
-from src.mode import Mode
-from src.train import Train
-import pickle
-import pandas as pd
-import os
-
-"""
-The Strategists - Analysis Model's entry point for all the analysis-related actions.
-"""
+from src import model
 
 parser = ArgumentParser(description='Entry point to train and execute the model.')
-parser.add_argument('--mode', '-m', type=Mode, choices=list(Mode), required=True)
-parser.add_argument('--data-directory', '-D', type=str, help='Directory where game exports are saved', required=True)
-parser.add_argument('--model-directory', '-M', type=str, help='Directory where model will be exported', required=True)
-parser.add_argument('--predict-file-directory', '-P', type=str, help='Directory where prediction file will be stored',
-                    required=True)
+subparsers = parser.add_subparsers(dest='subcommand', required=True)
+
+parser_train = subparsers.add_parser('train')
+parser_train.add_argument('--data-dir', '-D', type=str, help='Directory where game exports are saved', required=True)
+parser_train.add_argument('--out-dir', '-O', type=str, help='Directory where model will be exported', required=True)
+
+parser_predict = subparsers.add_parser('predict')
+parser_predict.add_argument('--model-dir', '-M', type=str, help='Directory where model is exported', required=True)
+parser_predict.add_argument('--predict_file', '-P', type=str, help='Path to prediction CSV file', required=True)
+
 args = parser.parse_args()
-
-MODEL_FILE_NAME = 'boosting_model.sav'
-
-
-def train_model():
-    train = Train(args.data_directory, args.model_directory)
-    train.prepare_model()
-
-
-def predict_outcome():
-    loaded_model = pickle.load(open(os.path.join(args.model_directory, MODEL_FILE_NAME), 'rb'))
-
-    csv_file_path = args.predict_file_directory
-    input_df = pd.read_csv(csv_file_path)
-
-    debit_columns = [col for col in input_df.columns if col.startswith('debit.')]
-    credit_columns = [col for col in input_df.columns if col.startswith('credit.')]
-
-    features = ['player.base-cash'] + debit_columns + credit_columns
-    input_df_filtered = input_df[features]
-
-    predicted_probability = loaded_model.predict_proba(input_df_filtered)[:, 1]
-
-    threshold = 0.2
-    prediction = (predicted_probability >= threshold).astype(int)
-
-    print("The predicted outcome is:", prediction[0])
 
 
 if __name__ == '__main__':
-    if args.mode == Mode.TRAIN:
-        train_model()
+    if args.subcommand == 'train':
+        model.export_model(args.data_dir, args.out_dir)
     else:
-        predict_outcome()
+        model.evaluate_model(args.model_dir, args.predict_file)
