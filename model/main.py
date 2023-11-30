e77c5b92bc8cb4f97b34a01c0bc101ea35847f68
Himanshu Sharma <himanshusharma7296@gmail.com>
2023-11-29 17:28:39 -0700

    Added boosting model

--- model/main.py
+++ model/main.py
@@ -3,6 +3,9 @@
 from argparse import ArgumentParser
 from src.mode import Mode
 from src.train import Train
+import pickle
+import pandas as pd
+import os
 
 """
 The Strategists - Analysis Model's entry point for all the analysis-related actions.
@@ -12,14 +15,37 @@ parser = ArgumentParser(description='Entry point to train and execute the model.
 parser.add_argument('--mode', '-m', type=Mode, choices=list(Mode), required=True)
 parser.add_argument('--data-directory', '-D', type=str, help='Directory where game exports are saved', required=True)
 parser.add_argument('--model-directory', '-M', type=str, help='Directory where model will be exported', required=True)
+parser.add_argument('--predict-file-directory', '-P', type=str, help='Directory where prediction file will be stored',
+                    required=True)
 args = parser.parse_args()
+
+MODEL_FILE_NAME = 'boosting_model.sav'
+
 
 def train_model():
     train = Train(args.data_directory, args.model_directory)
     train.prepare_model()
 
+
 def predict_outcome():
-    pass
+    loaded_model = pickle.load(open(os.path.join(args.model_directory, MODEL_FILE_NAME), 'rb'))
+
+    csv_file_path = args.predict_file_directory
+    input_df = pd.read_csv(csv_file_path)
+
+    debit_columns = [col for col in input_df.columns if col.startswith('debit.')]
+    credit_columns = [col for col in input_df.columns if col.startswith('credit.')]
+
+    features = ['player.base-cash'] + debit_columns + credit_columns
+    input_df_filtered = input_df[features]
+
+    predicted_probability = loaded_model.predict_proba(input_df_filtered)[:, 1]
+
+    threshold = 0.2
+    prediction = (predicted_probability >= threshold).astype(int)
+
+    print("The predicted outcome is:", prediction[0])
+
 
 if __name__ == '__main__':
     if args.mode == Mode.TRAIN:
--- model/src/train.py
+++ model/src/train.py
@@ -1,9 +1,17 @@
-from os import listdir, access, W_OK
+from os import listdir, access, W_OK, path
 from os.path import exists, isdir
+import pickle
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.utils.class_weight import compute_sample_weight
 
 """
 This file contains the class to train the model.
 """
+
+MODEL_FILE_NAME = 'boosting_model.sav'
+
 
 class Train:
     def __init__(self, data_directory: str, model_directory: str) -> None:
@@ -21,11 +29,46 @@ class Train:
         # checking if data directory exists
         if not exists(self.__data_directory__):
             raise OSError(f'Data directory {self.__data_directory__} doesn\'t exists!')
-        
+
         # checking if model directory exists or the script can create
         if not access(self.__model_directory__, W_OK):
             raise OSError(f'Model directoy {self.__model_directory__} doesn\'t have write access!')
 
     def prepare_model(self) -> None:
+        data_files = []
         for i, filename in enumerate(listdir(self.__data_directory__)):
+            data_files.append(path.join(self.__data_directory__, filename))
             print(f'{i} - {filename}')
+
+        all_games_data = pd.concat([pd.read_csv(f) for f in data_files], ignore_index=True)
+
+        all_games_data['is_winner'] = all_games_data['player.state'].apply(lambda x: 1 if x == 'ACTIVE' else 0)
+
+        debit_columns = [col for col in all_games_data.columns if col.startswith('debit.')]
+
+        credit_columns = [col for col in all_games_data.columns if col.startswith('credit.')]
+
+        features = ['player.base-cash'] + debit_columns + credit_columns
+
+        X = all_games_data[features]
+        y = all_games_data['is_winner']
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+        sample_weights = compute_sample_weight(class_weight='balanced', y=y_train)
+
+        param_grid = {
+            'n_estimators': [50, 100, 200],
+            'max_depth': [3, 5, 10],
+            'min_samples_leaf': [1, 2, 4]
+        }
+
+        grid_search = GridSearchCV(estimator=GradientBoostingClassifier(random_state=42),
+                                   param_grid=param_grid,
+                                   scoring='f1',
+                                   cv=5)
+
+        grid_search.fit(X_train, y_train, sample_weight=sample_weights)
+
+        filename = path.join(self.__model_directory__, MODEL_FILE_NAME)
+        pickle.dump(grid_search.best_estimator_, open(filename, 'wb'))
