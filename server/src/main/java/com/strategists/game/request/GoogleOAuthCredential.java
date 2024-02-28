96441fa41474bb9450e6c816918c47894cd4f34e
Shubham Chawla <shubham_chawla@live.com>
2024-02-28 00:32:43 -0700

    implemented resume functionality

--- server/src/main/java/com/strategists/game/request/GoogleOAuthCredential.java
+++ server/src/main/java/com/strategists/game/request/GoogleOAuthCredential.java
@@ -1,17 +1,27 @@
 package com.strategists.game.request;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.strategists.game.util.GoogleOAuthCredentialDeserializer;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.val;
 
 @Getter
 @AllArgsConstructor
 @JsonDeserialize(using = GoogleOAuthCredentialDeserializer.class)
 public class GoogleOAuthCredential {
 
+	private static final String JSON_FORMAT = "{\"credential\":\"%s\"}";
+
 	private String name;
 	private String email;
+
+	public static GoogleOAuthCredential fromJWT(String jwtString) throws JsonProcessingException {
+		val mapper = new ObjectMapper();
+		return mapper.readValue(String.format(JSON_FORMAT, jwtString), GoogleOAuthCredential.class);
+	}
 
 }
