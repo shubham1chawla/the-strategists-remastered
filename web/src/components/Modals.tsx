f436b0fed8c379613bde689d152aa9b404aa2503
Shubham Chawla <shubham_chawla@live.com>
2024-04-05 01:36:28 -0700

    added error checking on investment and skip actions

--- web/src/components/Modals.tsx
+++ web/src/components/Modals.tsx
@@ -15,6 +15,7 @@ import {
   Statistic,
   Tabs,
   Tag,
+  notification,
 } from 'antd';
 import { SliderMarks } from 'antd/es/slider';
 import {
@@ -80,144 +81,177 @@ export interface PlayerInvestModalProps extends BaseModalProps {
 export const PlayerInvestModal = (props: Partial<PlayerInvestModalProps>) => {
   const { open, gameCode, player, land, title, onCancel } = props;
   const [ownership, setOwnership] = useState(0);
+  const [investing, setInvesting] = useState(false);
+  const [api, contextHolder] = notification.useNotification();
+
+  // Validating props
   if (!open || !gameCode || !player || !land || !title || !onCancel) {
+    return null;
+  }
+
+  // Closing the modal if player doesn't have the turn
+  if (!player.turn) {
+    onCancel();
     return null;
   }
 
   // Setting up investment strategy
   const strategy = new InvestmentStrategy(player, land, ownership);
 
-  const invest = async () => {
-    await axios.post(`/api/games/${gameCode}/players/${player.id}/lands`, {
-      landId: land.id,
-      ownership,
-    });
-    onModalCancel();
+  const onInvest = async () => {
+    setInvesting(true);
+    try {
+      // Investing in the land
+      await axios.post(`/api/games/${gameCode}/players/${player.id}/lands`, {
+        landId: land.id,
+        ownership,
+      });
 
-    // Ending player's turn after investing in any land
-    axios.put(`/api/games/${gameCode}/turn`);
+      // Ending player's turn after investing in any land
+      await axios.put(`/api/games/${gameCode}/turn`);
+
+      // Closing the modal
+      onModalCancel();
+    } catch (error) {
+      console.error(error);
+      api.error({
+        message: 'Something went wrong!',
+        description: 'Please refresh the page and try again.',
+      });
+    } finally {
+      setInvesting(false);
+    }
   };
 
   // Ensuring that if model closes, we are resetting states
   const onModalCancel = () => {
     setOwnership(0);
+    setInvesting(false);
     onCancel();
   };
 
   return (
-    <Modal
-      className="strategists-actions__modal"
-      title="Investment Strategy"
-      open={!!open}
-      onCancel={onModalCancel}
-      footer={
-        <Row justify="space-between" wrap={false}>
-          <Space>
-            {strategy.maxOfferableOwnership !== strategy.availableOwnership ? (
-              <>
-                <ExclamationCircleOutlined />
-                <span>
-                  Offer capped at {strategy.maxOfferableOwnership}% due to low
-                  cash!
-                </span>
-              </>
-            ) : (
-              ''
-            )}
-          </Space>
-          <Space>
-            <Button onClick={onModalCancel}>Cancel</Button>
-            <Button
-              type="primary"
-              disabled={!strategy.feasible}
-              icon={<RiseOutlined />}
-              onClick={invest}
-            >
-              Invest
-            </Button>
-          </Space>
-        </Row>
-      }
-    >
-      <LandStats land={land} />
-      <Row>
-        <Col span={12}>
-          <Card bordered={false}>
-            <Statistic
-              title={
-                <Space>
-                  <PieChartOutlined />
-                  Proposed Ownership
-                </Space>
-              }
-              value={ownership}
-              precision={0}
-              suffix={<PercentageOutlined />}
-            />
-          </Card>
-        </Col>
-        <Col span={12}>
-          <Card bordered={false}>
-            <Statistic
-              title={
-                <Space>
-                  <WalletOutlined />
-                  Investment Cost
-                </Space>
-              }
-              value={strategy.cost}
-              precision={2}
-              prefix={<DollarCircleOutlined />}
-            />
-          </Card>
-        </Col>
-      </Row>
-      <Card className="strategists-actions__modal__slider-card">
-        <Slider
-          defaultValue={ownership}
-          min={0}
-          max={strategy.maxOfferableOwnership}
-          onChangeComplete={(value) => setOwnership(value)}
-          tooltip={{
-            formatter: (value) => `${value}%`,
-          }}
-          autoFocus={true}
-          marks={prepareSliderMarks(strategy)}
-        />
-        <Row justify="center">
-          <Space>
-            <InfoCircleOutlined />
-            Use this slider to adjust your investment offer.
-          </Space>
-        </Row>
-      </Card>
-      <Collapse
-        size="large"
-        bordered={false}
-        ghost={true}
-        expandIconPosition="end"
-        items={[
-          {
-            key: '1',
-            label:
-              land.totalOwnership > 0 ? (
-                <Space>
-                  <SlidersOutlined />
-                  <span>Click to check {land.name}'s investments</span>
-                </Space>
+    <>
+      {contextHolder}
+      <Modal
+        className="strategists-actions__modal"
+        title="Investment Strategy"
+        open={!!open}
+        onCancel={onModalCancel}
+        footer={
+          <Row justify="space-between" wrap={false}>
+            <Space>
+              {strategy.maxOfferableOwnership !==
+              strategy.availableOwnership ? (
+                <>
+                  <ExclamationCircleOutlined />
+                  <span>
+                    Offer capped at {strategy.maxOfferableOwnership}% due to low
+                    cash!
+                  </span>
+                </>
               ) : (
-                <Space>
-                  <CheckCircleOutlined />
-                  <span>No investments in {land.name}!</span>
-                </Space>
+                ''
+              )}
+            </Space>
+            <Space>
+              <Button onClick={onModalCancel}>Cancel</Button>
+              <Button
+                type="primary"
+                disabled={!strategy.feasible || investing}
+                icon={<RiseOutlined />}
+                onClick={onInvest}
+                loading={investing}
+              >
+                Invest
+              </Button>
+            </Space>
+          </Row>
+        }
+      >
+        <LandStats land={land} />
+        <Row>
+          <Col span={12}>
+            <Card bordered={false}>
+              <Statistic
+                title={
+                  <Space>
+                    <PieChartOutlined />
+                    Proposed Ownership
+                  </Space>
+                }
+                value={ownership}
+                precision={0}
+                suffix={<PercentageOutlined />}
+              />
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card bordered={false}>
+              <Statistic
+                title={
+                  <Space>
+                    <WalletOutlined />
+                    Investment Cost
+                  </Space>
+                }
+                value={strategy.cost}
+                precision={2}
+                prefix={<DollarCircleOutlined />}
+              />
+            </Card>
+          </Col>
+        </Row>
+        <Card className="strategists-actions__modal__slider-card">
+          <Slider
+            defaultValue={ownership}
+            min={0}
+            max={strategy.maxOfferableOwnership}
+            onChangeComplete={(value) => setOwnership(value)}
+            tooltip={{
+              formatter: (value) => `${value}%`,
+            }}
+            autoFocus={true}
+            marks={prepareSliderMarks(strategy)}
+          />
+          <Row justify="center">
+            <Space>
+              <InfoCircleOutlined />
+              Use this slider to adjust your investment offer.
+            </Space>
+          </Row>
+        </Card>
+        <Collapse
+          size="large"
+          bordered={false}
+          ghost={true}
+          expandIconPosition="end"
+          items={[
+            {
+              key: '1',
+              label:
+                land.totalOwnership > 0 ? (
+                  <Space>
+                    <SlidersOutlined />
+                    <span>Click to check {land.name}'s investments</span>
+                  </Space>
+                ) : (
+                  <Space>
+                    <CheckCircleOutlined />
+                    <span>No investments in {land.name}!</span>
+                  </Space>
+                ),
+              children: (
+                <TabularPortfolio
+                  perspective="land"
+                  playerLands={land.players}
+                />
               ),
-            children: (
-              <TabularPortfolio perspective="land" playerLands={land.players} />
-            ),
-          },
-        ]}
-      />
-    </Modal>
+            },
+          ]}
+        />
+      </Modal>
+    </>
   );
 };
 
