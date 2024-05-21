f503199dad5cb3fc79a0490a5e77077596a9d5a9
Shubham Chawla <shubham_chawla@live.com>
2024-05-21 21:25:36 +0530

    refactored predictions configuration properties

--- server/src/main/java/com/strategists/game/configuration/properties/GoogleUtilsPermissionsConfigurationProperties.java
+++ server/src/main/java/com/strategists/game/configuration/properties/GoogleUtilsPermissionsConfigurationProperties.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.nio.file.Paths;
 
 import javax.validation.Valid;
-import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotBlank;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -14,16 +13,8 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 @ConfigurationProperties(prefix = "strategists.google.utils.permissions")
 public record GoogleUtilsPermissionsConfigurationProperties(@NotBlank String command,
-		@DefaultValue @Valid Export export, @DefaultValue @Valid Spreadsheet spreadsheet) {
-
-	public record Export(File directory, @NotBlank String fileName) {
-
-		@AssertTrue(message = "Path must be a directory and must exists!")
-		boolean isDirectoryValid() {
-			return directory.exists() && directory.isDirectory();
-		}
-
-	}
+		@DefaultValue @Valid SimpleFileExportConfigurationProperties export,
+		@DefaultValue @Valid Spreadsheet spreadsheet) {
 
 	public record Spreadsheet(@NotBlank String id, @NotBlank String range) {
 
