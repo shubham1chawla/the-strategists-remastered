6a878c7dc94ac9e6633768e4bbca771d6e8ee9f7
Shubham Chawla <shubham_chawla@live.com>
2024-05-25 15:59:02 +0530

    added bypass functionality to predictions

--- server/src/main/java/com/strategists/game/configuration/properties/GoogleUtilsPredictionsConfigurationProperties.java
+++ server/src/main/java/com/strategists/game/configuration/properties/GoogleUtilsPredictionsConfigurationProperties.java
@@ -5,8 +5,8 @@ import javax.validation.constraints.NotBlank;
 
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
-public record GoogleUtilsPredictionsConfigurationProperties(@NotBlank String command,
-		@DefaultValue @Valid SubCommandConfigurationProperties download,
+public record GoogleUtilsPredictionsConfigurationProperties(boolean bypassGoogleDriveSyncForTesting,
+		@NotBlank String command, @DefaultValue @Valid SubCommandConfigurationProperties download,
 		@DefaultValue @Valid SubCommandConfigurationProperties upload) {
 
 	public record SubCommandConfigurationProperties(@NotBlank String subCommand, @NotBlank String driveFolderId) {
