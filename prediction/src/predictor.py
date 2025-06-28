3a77b8273745867757f9a09362e796b9ebf5bc90
Shubham Chawla <shubham_chawla@live.com>
2025-06-27 23:17:47 -0400

    Upgraded prediction package's python dependencies, fixed source code issues, and migrated from poetry to uv

--- prediction/src/predictor.py
+++ prediction/src/predictor.py
@@ -1,24 +1,21 @@
-# Standard Python imports
-import time
 import json
 import logging as log
-from os import listdir, path
-from typing import final
+import time
 from dataclasses import dataclass
 from datetime import datetime
+from os import listdir, path
+from typing import final
 
-# Machine-learning imports
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold, train_test_split
+import numpy as np
+import pandas as pd
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_auc_score, roc_curve
+from sklearn.model_selection import KFold, train_test_split
 from sklearn.utils.class_weight import compute_sample_weight
 
-# Project imports
-from .configuration import PredictorConfiguration
 from .classifiers import PredictorClassifierType, PredictorClassifier
 from .classifiers import get_predictor_classifiers
+from .configuration import PredictorConfiguration
 
 
 @final
@@ -36,14 +33,12 @@ class PredictorMetadata:
     cvs_files_count: int
     rows_count: int
 
-
     def __init__(self, configuration: PredictorConfiguration, classifier: PredictorClassifier) -> None:
         self.timestamp = int(round(datetime.now().timestamp() * 1000))
         self.configuration = configuration
-        self.classifier_type = classifier.type
-        self.score = classifier.best_score_
-        self.params = classifier.best_params_
-
+        self.classifier_type = classifier.prediction_classifier_type
+        self.score = classifier.best_score
+        self.params = classifier.best_params
 
     def describe(self) -> None:
         log.info(f'CSV Files: {self.cvs_files_count} | Rows: {self.rows_count}')
@@ -55,8 +50,7 @@ class PredictorMetadata:
         log.info(f'Classification Report:\n{self.classification_report}')
         log.info(f'Confusion Matrix:\n{self.confusion_matrix}')
 
-    
-    def export(self) -> None:        
+    def export(self) -> None:
         export_file_path, exportable_dict = self.export_file_path, self.__dict__
         exportable_dict['classifier_type'] = self.classifier_type.name
         exportable_dict['configuration'] = self.configuration.__dict__
@@ -67,11 +61,9 @@ class PredictorMetadata:
             file.write(json.dumps(exportable_dict, indent=4))
             log.info(f'Exported metadata: {export_file_path}')
 
-
     @property
     def export_file_path(self) -> str:
         return path.join(self.configuration.metadata_directory, self.file_name)
-
 
     @property
     def file_name(self) -> str:
@@ -89,10 +81,8 @@ class Prediction:
 
 class Predictor:
 
-
     def __init__(self, configuration: PredictorConfiguration) -> None:
-        self.__configuration__ = configuration
-    
+        self.configuration_ = configuration
 
     def export_model(self) -> None:
         start_time = time.time()
@@ -101,52 +91,61 @@ class Predictor:
         df, csv_files_count, rows_count = self.import_data()
 
         # Feature extraction
-        X = self.extract_features(df)
-        y = df['player.state'].apply(lambda x: 1 if x == 'ACTIVE' else 0)
-    
+        x = self.extract_features(df)
+        y = df['player.state'].apply(lambda state: 1 if state == 'ACTIVE' else 0)
+
         # Splitting the dataset into train and test
-        test_size, random_state = self.__configuration__.TEST_SIZE, self.__configuration__.RANDOM_STATE
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+        test_size, random_state = self.configuration_.TEST_SIZE, self.configuration_.RANDOM_STATE
+        x_train, x_test, y_train, y_test = train_test_split(x, y,
+                                                            test_size=test_size,
+                                                            random_state=random_state,
+                                                            stratify=y)
 
         # Validating classifiers using 5-fold method
         log.info('Estimating best classifier...')
         best_classifier, best_mean_train_accuracy, best_mean_val_accuracy = None, 0, 0
-        for classifier in get_predictor_classifiers(self.__configuration__):
+        for classifier in get_predictor_classifiers(self.configuration_):
 
-            n_splits = self.__configuration__.KFOLD_N_SPLITS
+            n_splits = self.configuration_.KFOLD_N_SPLITS
             kf = KFold(n_splits=n_splits, shuffle=True, random_state=random_state)
 
             train_accuracies, val_accuracies = [], []
-            for _, (train_indexes, val_indexes) in enumerate(kf.split(X_train)):
-
+            for _, (train_indexes, val_indexes) in enumerate(kf.split(x_train)):
                 # Extracting folded train and validation rows
-                X_train_, y_train_ = X_train.iloc[train_indexes], y_train.iloc[train_indexes]
-                X_val_, y_val_ = X_train.iloc[val_indexes], y_train.iloc[val_indexes]
+                x_train_, y_train_ = x_train.iloc[train_indexes], y_train.iloc[train_indexes]
+                x_val_, y_val_ = x_train.iloc[val_indexes], y_train.iloc[val_indexes]
 
                 # Fitting model on folded dataset and getting train & validation accuracies
-                class_weight = self.__configuration__.SAMPLE_WEIGHT_CLASS
-                classifier.fit(X_train_, y_train_, compute_sample_weight(class_weight=class_weight, y=y_train_))
-                train_accuracies.append(accuracy_score(y_train_, classifier.predict(X_train_)))
-                val_accuracies.append(accuracy_score(y_val_, classifier.predict(X_val_)))
+                class_weight = self.configuration_.SAMPLE_WEIGHT_CLASS
+                classifier.fit(x_train_, y_train_, compute_sample_weight(class_weight=class_weight, y=y_train_))
+                train_accuracies.append(accuracy_score(y_train_, classifier.predict(x_train_)))
+                val_accuracies.append(accuracy_score(y_val_, classifier.predict(x_val_)))
 
             mean_train_accuracy, mean_val_accuracy = np.mean(train_accuracies), np.mean(val_accuracies)
-            log.info(f'Classifier: {classifier.type.name:<18} | Train Accuracy (Avg): {mean_train_accuracy:.2f} | Validation Accuracy (Avg): {mean_val_accuracy:.2f}')
+            log.info(
+                f'Classifier: {classifier.prediction_classifier_type.name:<18} | '
+                f'Train Accuracy (Avg): {mean_train_accuracy:.2f} | '
+                f'Validation Accuracy (Avg): {mean_val_accuracy:.2f}'
+            )
 
             delta = abs(mean_val_accuracy - best_mean_val_accuracy)
-            if (delta < 0.01 and mean_train_accuracy > best_mean_train_accuracy) or (mean_val_accuracy > best_mean_val_accuracy):
+            if (
+                    (delta < 0.01 and mean_train_accuracy > best_mean_train_accuracy) or
+                    (mean_val_accuracy > best_mean_val_accuracy)
+            ):
                 best_classifier = classifier
                 best_mean_train_accuracy = mean_train_accuracy
                 best_mean_val_accuracy = mean_val_accuracy
-            
+
         # Training & testing best classifier on complete dataset
-        class_weight = self.__configuration__.SAMPLE_WEIGHT_CLASS
-        best_classifier.fit(X_train, y_train, compute_sample_weight(class_weight=class_weight, y=y_train))
-        y_pred, y_pred_probs = classifier.predict(X_test), classifier.predict_proba(X_test)[:, 1]
+        class_weight = self.configuration_.SAMPLE_WEIGHT_CLASS
+        best_classifier.fit(x_train, y_train, compute_sample_weight(class_weight=class_weight, y=y_train))
+        y_pred, y_pred_probs = best_classifier.predict(x_test), best_classifier.predict_proba(x_test)[:, 1]
         test_accuracy = accuracy_score(y_test, y_pred)
         roc_auc = roc_auc_score(y_test, y_pred_probs)
 
         # Preparing metadata
-        metadata = PredictorMetadata(self.__configuration__, best_classifier)
+        metadata = PredictorMetadata(self.configuration_, best_classifier)
         metadata.cvs_files_count = csv_files_count
         metadata.rows_count = rows_count
         metadata.test_accuracy = test_accuracy
@@ -155,21 +154,25 @@ class Predictor:
         metadata.confusion_matrix = confusion_matrix(y_test, y_pred)
 
         # Logging metadata
-        metadata.describe()    
-        if self.__configuration__.show_plots:
-            self.plot_roc_curve(y_test, y_pred_probs, roc_auc)
+        metadata.describe()
+        if self.configuration_.show_plots:
+            Predictor.plot_roc_curve(y_test, y_pred_probs, roc_auc)
 
         # Exporting metadata
         metadata.export()
 
         # Exporting model's pickle file
-        pickle_file_path, search_method = self.__configuration__.pickle_file_path, self.__configuration__.search_method
+        pickle_file_path, search_method = self.configuration_.pickle_file_path, self.configuration_.search_method
         best_classifier.export(pickle_file_path)
         end_time = time.time() - start_time
-        log.info(f'Export Path: {pickle_file_path} | Export Time: {end_time:.02f} seconds | Search Method: {search_method}')
+        log.info(
+            f'Export Path: {pickle_file_path} | '
+            f'Export Time: {end_time:.02f} seconds | '
+            f'Search Method: {search_method}'
+        )
 
-
-    def plot_roc_curve(self, y_test, y_pred_probs, roc_auc) -> None:
+    @staticmethod
+    def plot_roc_curve(y_test, y_pred_probs, roc_auc) -> None:
         fpr, tpr, _ = roc_curve(y_test, y_pred_probs)
         plt.figure()
         plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
@@ -182,57 +185,69 @@ class Predictor:
         plt.legend(loc='lower right')
         plt.show()
 
-    
     def execute_model(self) -> None:
-        classifier = PredictorClassifier.load(self.__configuration__.pickle_file_path)
-        log.info(f'Loaded classifier: {classifier.type.name}')
+        classifier = PredictorClassifier.load(self.configuration_.pickle_file_path)
+        log.info(f'Loaded classifier: {classifier.prediction_classifier_type.name}')
 
-        test_file_path = self.__configuration__.test_file_path
+        test_file_path = self.configuration_.test_file_path
         test_file_dir = path.dirname(test_file_path)
         game_code = path.basename(test_file_path).split('.')[0]
 
         df = pd.read_csv(test_file_path)
         predictions = []
         for player_id, row in df.groupby(by='player.id'):
-            X = self.extract_features(row)
-            prediction = Prediction(game_code, 
-                                    player_id, 
-                                    classifier.predict_proba(X)[0].tolist(), 
-                                    int(classifier.predict(X)[0]))
+            player_id: int
+            x = Predictor.extract_features(row)
+            prediction = Prediction(game_code,
+                                    player_id,
+                                    classifier.predict_proba(x)[0].tolist(),
+                                    int(classifier.predict(x)[0]))
             predictions.append(prediction)
 
         # Finding test file directory
         prediction_file_path = path.join(test_file_dir, f'{game_code}.json')
-        predictions_json = json.dumps([ prediction.__dict__ for prediction in predictions ], indent=4)
+        predictions_json = json.dumps([prediction.__dict__ for prediction in predictions], indent=4)
         with open(prediction_file_path, 'w') as file:
             file.write(predictions_json)
-            log.info(f'Exported predctions to: {prediction_file_path}')
+            log.info(f'Exported predictions to: {prediction_file_path}')
 
-
-    def extract_features(self, df: pd.DataFrame) -> pd.DataFrame:
+    @staticmethod
+    def extract_features(df: pd.DataFrame) -> pd.DataFrame:
         features = df.copy()
-        features['ownership.average'] = features['ownership.total'] / (features['ownership.count'] * 100)
-        features['debit.invest.average'] = features['debit.invest.total'] / (features['debit.invest.count'] * df['player.base-cash'])
-        features['debit.rent.average'] = features['debit.rent.total'] / (features['debit.rent.count'] * df['player.base-cash'])
-        features['credit.rent.average'] = features['credit.rent.total'] / (features['credit.rent.count'] * df['player.base-cash'])
+        features['ownership.average'] = (
+                features['ownership.total'] / (features['ownership.count'] * 100)
+        )
+        features['debit.invest.average'] = (
+                features['debit.invest.total'] / (features['debit.invest.count'] * df['player.base-cash'])
+        )
+        features['debit.rent.average'] = (
+                features['debit.rent.total'] / (features['debit.rent.count'] * df['player.base-cash'])
+        )
+        features['credit.rent.average'] = (
+                features['credit.rent.total'] / (features['credit.rent.count'] * df['player.base-cash'])
+        )
         for col in df.columns:
             if col.startswith('ownership') and not (col.endswith('total') or col.endswith('count')):
                 features[col] /= 100
-            elif (col.startswith('debit') or col.startswith('credit')) and not (col.endswith('total') or col.endswith('count')):
+            elif (
+                    (col.startswith('debit') or col.startswith('credit')) and
+                    not (col.endswith('total') or col.endswith('count'))
+            ):
                 features[col] /= df['player.base-cash']
             else:
                 features = features.drop(col, axis=1)
         return features.fillna(0)
-    
 
     def import_data(self) -> tuple[pd.DataFrame, int, int]:
 
         # Finding all the CSV files present in the data directory
-        data_directory, csv_files = self.__configuration__.data_directory, []
+        data_directory, csv_files, dfs = self.configuration_.data_directory, [], []
         for export_file_path in listdir(data_directory):
             if export_file_path.endswith('.csv'):
-                csv_files.append(path.join(data_directory, export_file_path))
-        
+                csv_file: str = path.join(data_directory, export_file_path)
+                csv_files.append(csv_file)
+                dfs.append(pd.read_csv(csv_file))
+
         # Combining all the CSV files as one dataframe
-        df = pd.concat([pd.read_csv(f) for f in csv_files], ignore_index=True)
+        df = pd.concat(dfs, ignore_index=True)
         return df, len(csv_files), df.shape[0]
